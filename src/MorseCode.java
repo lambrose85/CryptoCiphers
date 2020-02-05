bce9d4896d3aafb4d36ff07b7b88d89438912187
Lucas Ambrose <lucasambrose@siu.edu>
2020-02-05 00:02:26 -0600

    added basic GUI

--- src/MorseCode.java
+++ src/MorseCode.java
@@ -1,9 +1,22 @@
-
+import java.io.*;
+import 
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
 public class MorseCode {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String plainText;
+	private String cipherText;
+	public static void main(String[] args) throws Exception{
+	 
+		//select a file
+		JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+		int returnValue = jfc.showOpenDialog(null);
+		
+		if(returnValue == JFileChooser.APPROVE_OPTION) {
+			//returns absolute path of selected file
+			File SelectedFile = jfc.getSelectedFile();
+			System.out.println(SelectedFile.getAbsolutePath());
+		}
+	
 	}
 
 }
